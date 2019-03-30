fb1d36d58d8b2d20fba45ffd9e42aeb877d61b22
ethan <ethanwang9011@email.com>
2019-03-31 08:09:22 +1300

    2019/3/31

---
+++ ConsoleReview1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleReview1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello age");
+            Console.WriteLine("test{0},{2}",1, 2, 3);
+        }
+    }
+}
